22c30e3cc7129128aeb8f7f6942699bd40e0305b
Tlotliso <tlotlisomokati@gmail.com>
2024-05-24 15:00:44 +0200

    Added User Profile

--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -37,8 +37,107 @@
                                     </div>
                                 </div>
                             </div>
+                            <div class="row gx-10 mb-5">
+                                <div class="col-lg-6">
+                                    <div class="mb-5">
+                                        {{ Form::label('first_name', __('messages.client.first_name').':', ['class' => 'form-label required mb-3']) }}
+                                        {{ Form::text('first_name', $user->first_name ?? null, ['class' => 'form-control form-control-solid', 'placeholder' => __('messages.client.first_name'), 'required']) }}
+                                    </div>
+                                </div>
+                                <div class="col-lg-6">
+                                    <div class="mb-5">
+                                        {{ Form::label('last_name', __('messages.client.last_name').':', ['class' => 'form-label required mb-3']) }}
+                                        {{ Form::text('last_name', $user->last_name ?? null, ['class' => 'form-control form-control-solid', 'placeholder' => __('messages.client.last_name'), 'required']) }}
+                                    </div>
+                                </div>
+                                <div class="col-lg-6">
+                                <div class="mb-5">
+                                    {{ Form::label('email', __('messages.client.email').':', ['class' => 'form-label required mb-3']) }}
+                                    {{ Form::email('email', $user->email ?? null, ['class' => 'form-control form-control-solid', 'placeholder' => __('messages.client.email'), 'required']) }}
+                                </div>
+                            </div>
+                            <div class="col-lg-6">
+                                <div class="mb-5">
+                                    {{ Form::label('contact', __('messages.client.contact_no').':', ['class' => 'form-label mb-3']) }}
+                                    {{ Form::tel('contact', $user->contact ??  getSettingValue('country_code'), ['class' => 'form-control form-control-solid', 'onkeyup' => 'if (/\D/g.test(this.value)) this.value = this.value.replace(/\D/g,"")','id'=>'phoneNumber']) }}
+                                    {{ Form::hidden('region_code', $user->region_code ?? null, ['id'=>'prefix_code']) }}
+                                    <span id="valid-msg" class="hide text-success fw-400 fs-small mt-2">✓ &nbsp; {{ __('messages.placeholder.valid_number') }}</span>
+                                    <span id="error-msg" class="hide text-danger fw-400 fs-small mt-2"></span>
+                                </div>
+                            </div>
+                                <div class="col-lg-6">
+                                    <div class="mb-5">
+                                        {{ Form::label('Country', __('messages.client.country').':', ['class' => 'form-label required mb-3']) }}
+                                        {{ Form::text('Country', $user->region ?? null, ['class' => 'form-control form-control-solid', 'placeholder' => __('messages.client.country'), 'required']) }}
+                                    </div>
+                                </div>
+                                <div class="col-lg-6">
+                                    <div class="mb-5">
+                                        {{ Form::label('date_of_birth', __('Date Of Birth').':', ['class' => 'form-label required mb-3']) }}
+                                        {{ Form::date('date_of_birth', $user->date_of_birth ?? null, ['class' => 'form-control form-control-solid', 'placeholder' => __('messages.client.date_of_birth'), 'required']) }}
+                                    </div>
+                                </div>
 
-                            <div class="row mb-6">
+                                <div class="col-lg-6">
+                                    <div class="mb-5">
+                                        {{ Form::label('Practise', __('Practice').':', ['class' => 'form-label required mb-3']) }}
+                                        {{ Form::text('Practise', $user->practice ?? null, ['class' => 'form-control form-control-solid', 'placeholder' => __('messages.client.practise'), 'required']) }}
+                                    </div>
+                                </div>
+                                <div class="col-lg-6">
+                                    <div class="mb-5">
+                                        {{ Form::label('Practice Number', __('Practice Number').':', ['class' => 'form-label required mb-3']) }}
+                                        {{ Form::text('practice_number', $user->practice_number ?? null, ['class' => 'form-control form-control-solid', 'placeholder' => __('messages.client.practice_number'), 'required']) }}
+                                    </div>
+                                </div>
+
+                                <div class="col-lg-6">
+                                    <div class="mb-5">
+                                        {{ Form::label('Address', __('messages.client.address').':', ['class' => 'form-label required mb-3']) }}
+                                        {{ Form::text('Address', $user->address ?? null, ['class' => 'form-control form-control-solid', 'placeholder' => __('messages.client.address'), 'required']) }}
+                                    </div>
+                                </div>
+                                <div class="col-lg-6">
+                                    <div class="mb-5">
+                                        {{ Form::label('zip_code', __('Zip Code').':', ['class' => 'form-label required mb-3']) }}
+                                        {{ Form::text('zip_code', $user->zip_code ?? null, ['class' => 'form-control form-control-solid', 'placeholder' => __('messages.client.zip_code'), 'required']) }}
+                                    </div>
+                                </div>
+
+                                <div class="col-lg-6">
+                                    <div class="mb-5">
+                                        {{ Form::label('State', __('messages.client.state').':', ['class' => 'form-label required mb-3']) }}
+                                        {{ Form::text('State', $user->state ?? null, ['class' => 'form-control form-control-solid', 'placeholder' => __('messages.client.state'), 'required']) }}
+                                    </div>
+                                </div>
+                                <div class="col-lg-6">
+                                    <div class="mb-5">
+                                        {{ Form::label('authorization_number', __('AuthoRization NUmber').':', ['class' => 'form-label required mb-3']) }}
+                                        {{ Form::text('authorization_number', $user->authorization_number ?? null, ['class' => 'form-control form-control-solid', 'placeholder' => __('messages.client.authorization_number'), 'required']) }}
+                                    </div>
+                                </div>
+
+                                 <div class="col-lg-4">
+                                    <div class="mb-5">
+                                        {{ Form::label('registration_number', __('Registration Number').':', ['class' => 'form-label required mb-3']) }}
+                                        {{ Form::text('registration_number', $user->registration_number ?? null, ['class' => 'form-control form-control-solid', 'placeholder' => __('messages.client.registration_number'), 'required']) }}
+                                    </div>
+                                </div>
+                                 <div class="col-lg-4">
+                                    <div class="mb-5">
+                                        {{ Form::label('license_number', __('License Number').':', ['class' => 'form-label required mb-3']) }}
+                                        {{ Form::text('license_number', $user->license_number ?? null, ['class' => 'form-control form-control-solid', 'placeholder' => __('messages.client.license_number'), 'required']) }}
+                                    </div>
+                                </div>
+                                 <div class="col-lg-4">
+                                    <div class="mb-5">
+                                        {{ Form::label('occupation', __('Occupation').':', ['class' => 'form-label required mb-3']) }}
+                                        {{ Form::text('occupation', $user->occupation ?? null, ['class' => 'form-control form-control-solid', 'placeholder' => __('messages.client.occupation'), 'required']) }}
+                                    </div>
+                                </div>
+                            </div>
+
+                            <!-- <div class="row mb-6">
                                 <label class="col-lg-4 form-label">{{ __('messages.user.full_name').':' }}</label>
                                 <div class="col-lg-8">
                                     <div class="row">
@@ -57,8 +156,8 @@
                                 <div class="col-lg-8 fv-row fv-plugins-icon-container">
                                     {!! Form::email('email', $user->email, ['class'=> 'form-control', 'placeholder' => __('messages.user.email'), 'required', 'id' => 'isEmailEditProfile']) !!}
                                 </div>
-                            </div>
-                            <div class="row mb-6">
+                            </div> -->
+                            <!-- <div class="row mb-6">
                                 <label class="col-lg-4 form-label mb-3">{{ __('messages.user.contact_number').':' }}</label>
                                 <div class="col-lg-8 fv-row fv-plugins-icon-container">
                                     {{ Form::tel('contact', isset($user)?$user->contact:null, ['class' => 'form-control','onkeyup' => 'if (/\D/g.test(this.value)) this.value = this.value.replace(/\D/g,"")','id'=>'phoneNumber', 'placeholder' => __('messages.user.phone_number')]) }}
@@ -66,7 +165,7 @@
                                     <span id="valid-msg" class="hide text-success fw-400 fs-small mt-2">✓ {{ __('messages.placeholder.valid_number') }}</span>
                                     <span id="error-msg" class="hide text-danger fw-400 fs-small mt-2"></span>
                                 </div>
-                            </div>
+                            </div> -->
 
                             @hasrole('admin')
                             <div class="float-end mb-6">
@@ -80,7 +179,7 @@
                                     <a href="{{route('client.dashboard')}}" type="reset"
                                        class="btn btn-secondary btn-active-light-primary">{{__('messages.common.discard')}}</a>
                                 </div>
-                                @endrole
+                            @endrole
                         </div>
                     </form>
                 </div>
