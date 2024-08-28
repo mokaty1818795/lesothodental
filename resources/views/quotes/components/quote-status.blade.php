d262294583bb0a5ff9d323a1797fdee7e5cc7c37
Tlotliso <tlotlisomokati@gmail.com>
2024-08-28 22:32:38 +0200

    changed the status of rejected application to be red

--- resources/views/quotes/components/quote-status.blade.php
+++ resources/views/quotes/components/quote-status.blade.php
@@ -1,5 +1,7 @@
 @if($row->status_label === 'Draft')
     <span class="badge bg-light-warning fs-7">{{$row->status_label}}</span>
+@elseif($row->status_label === 'Rejected')
+    <span class="badge bg-danger fs-7">{{$row->status_label}}</span>
 @else
-    <span class="badge bg-light-success fs-7">{{$row->status_label}}</span>
+ <span class="badge bg-light-success fs-7">{{$row->status_label}}</span>
 @endif
