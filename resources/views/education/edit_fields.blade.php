d439830517dea0215d067f1d5c56cfc49b734c6d
Tlotliso <tlotlisomokati@gmail.com>
2024-09-16 05:42:28 +0200

    Added the form labels to Edit Education form

---
+++ resources/views/education/edit_fields.blade.php
@@ -0,0 +1,62 @@
+<div class="row gx-10 mb-5">
+    <div class="col-lg-4 col-sm-12 mb-5">
+        <div class="mb-5">
+            {{ Form::label('institude', __('Institude').':', ['class' => 'form-label required mb-3']) }}
+            {{ Form::text('institude',"Thabo", ['class' => 'form-control form-control-solid', 'placeholder' => __('Institude'), 'required']) }}
+        </div>
+    </div>
+    <div class="col-lg-4 col-sm-12 mb-5">
+        <div class="mb-5">
+            {{ Form::label('course', __('Course').':', ['class' => 'form-label required mb-3']) }}
+            {{ Form::text('course',"Sello", ['class' => 'form-control form-control-solid', 'placeholder' => __('Course'), 'required']) }}
+        </div>
+    </div>
+    <div class="col-lg-4 col-sm-12 mb-5">
+        <div class="mb-5">
+            {{ Form::label('attended_from', __('Attended from').':', ['class' => 'form-label required mb-3']) }}
+            {{ Form::date('attended_from',"Thabo", ['class' => 'form-control form-control-solid', 'placeholder' => __('Attended from'), 'required']) }}
+        </div>
+    </div>
+     <div class="col-lg-4 col-sm-12 mb-5">
+        <div class="mb-5">
+            {{ Form::label('attended_to', __('Attended to').':', ['class' => 'form-label required mb-3']) }}
+            {{ Form::date('attended_to',"Thabo", ['class' => 'form-control form-control-solid', 'placeholder' => __('Attended to'), 'required']) }}
+        </div>
+    </div>
+    <div class="col-lg-4 col-sm-12 mb-5">
+        <div class="mb-5">
+            {{ Form::label('degree_date', __('Degree date"').':', ['class' => 'form-label required mb-3']) }}
+            {{ Form::date('degree_date',"Sello", ['class' => 'form-control form-control-solid', 'placeholder' => __('Degree date"'), 'required']) }}
+        </div>
+    </div>
+    <div class="col-lg-4 col-sm-12 mb-5">
+        <div class="mb-5">
+            {{ Form::label('specialization', __('Specialization').':', ['class' => 'form-label required mb-3']) }}
+            {{ Form::email('specialization',"Medicine", ['class' => 'form-control form-control-solid', 'placeholder' => __('Specialization'), 'required']) }}
+        </div>
+    </div>
+     <div class="col-lg-4 col-sm-12 mb-5">
+        <div class="mb-5">
+            {{ Form::label('telephone', __('Telephone').':', ['class' => 'form-label required mb-3']) }}
+            {{ Form::text('telephone',"22351290", ['class' => 'form-control form-control-solid', 'placeholder' => __('Telephone'), 'required']) }}
+        </div>
+    </div>
+    <div class="col-lg-4 col-sm-12 mb-5">
+        <div class="mb-5">
+            {{ Form::label('fax', __('Fax').':', ['class' => 'form-label required mb-3']) }}
+            {{ Form::text('fax',"+266 63809823", ['class' => 'form-control form-control-solid', 'placeholder' => __('Fax'), 'required']) }}
+        </div>
+    </div>
+    <div class="col-lg-4 col-sm-12 mb-5">
+        <div class="mb-5">
+            {{ Form::label('file', __('Certificate').':', ['class' => 'form-label required mb-3']) }}
+            {{ Form::file('file', ['class' => 'form-control form-control-solid', 'placeholder' => __('Certificate'), 'required']) }}
+        </div>
+    </div>
+</div>
+
+<div class="d-flex justify-content-end mt-5">
+    {{ Form::submit(__('messages.common.save'),['class' => 'btn btn-primary me-3']) }}
+    <a href="{{ route('client.education') }}" type="reset"
+       class="btn btn-secondary btn-active-light-primary">{{__('messages.common.discard')}}</a>
+</div>
--- resources/views/education/show_fields.blade.php
+++ resources/views/education/show_fields.blade.php
@@ -1,16 +1,34 @@
-<div class="card">
-    <div class="card-body">
-        <div class="row">
-            <div class="col-xxl-5 col-12">
-                <div class="d-sm-flex align-items-center mb-5 mb-xxl-0 text-center text-sm-start">
-                    <div class="image image-circle image-small">
-                       Hello
-                    </div>
-                    <div class="ms-0 ms-md-10 mt-5 mt-sm-0">
-                       Let Me show you Fileds HEre
+@extends('layouts.app')
+@section('title')
+    {{__('messages.client.client_details')}}
+@endsection
+@section('content')
+    @php $styleCss = 'style'; @endphp
+    <div class="container-fluid">
+        @include('flash::message')
+        <div class="d-flex flex-column">
+            <div class="col-12">
+                <div class="d-flex justify-content-between align-items-end mb-5">
+                    <h1>@yield('title')</h1>
+                    <a class="btn btn-outline-primary float-end"
+                       href="{{ route('client.education') }}">{{ __('messages.common.back') }}</a>
+                </div>
+                <div class="col-12">
+                    @include('layouts.errors')
+                </div>
+                <div class="card">
+                    <div class="card-body">
+                        {{ Form::open() }}
+                            @include('education.edit_fields')
+                        {{ Form::close() }}
                     </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
+@endsection
+@section('phone_js')
+    <script>
+        phoneNo = "{{ !empty($client) ? (($client->user->region_code).($client->user->contact)) : null }}";
+    </script>
+@endsection
